35e289e9b4e1f71fd5069d74f3805fb7f05554ae
SolForte <mateusbrandao1234@gmail.com>
2023-02-11 14:30:09 -0300

    Refactor: Code smells

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,34 +1,42 @@
 import { useState } from "react";
-export default function Usuario(props){
-    //Feito baseado no DrivenFlix
-    const fotoInicial = "assets/img/profiles/catanacomics.svg"
-    const [nomeDoUsuario, setNomeDoUsuario] = useState("catanacomics");
-    const [foto, setFoto] = useState(fotoInicial)
-    
-    function trocaNomeDoUsuario() {
-        const novoNome = prompt("Qual é o seu nome?")
-        if (novoNome.length!==0){
-            setNomeDoUsuario(novoNome)
-        }
+export default function Usuario() {
+  //Feito baseado no DrivenFlix
+  const fotoInicial = "assets/img/profiles/catanacomics.svg";
+  const [nomeDoUsuario, setNomeDoUsuario] = useState("catanacomics");
+  const [foto, setFoto] = useState(fotoInicial);
+
+  function trocaNomeDoUsuario() {
+    const novoNome = prompt("Qual é o seu nome?");
+    if (novoNome.length !== 0) {
+      setNomeDoUsuario(novoNome);
     }
+  }
 
-    function trocarFoto() {
-        const novaFoto = prompt("Insira o link da sua nova foto")
-        if (novaFoto.length!==0){
-          setFoto(novaFoto)
-        }  
+  function trocarFoto() {
+    const novaFoto = prompt("Insira o link da sua nova foto");
+    if (novaFoto.length !== 0) {
+      setFoto(novaFoto);
     }
+  }
 
-
-    return(
-        <div className="usuario">
-          <img data-test="profile-image" src={foto} alt="imagem de perfil" onClick={trocarFoto}/>
-          <div className="texto">
-            <span>
-              <strong data-test="name">{nomeDoUsuario}</strong>
-              <ion-icon data-test="edit-name" name="pencil" onClick={trocaNomeDoUsuario}></ion-icon>
-            </span>
-          </div>
-        </div>
-    )
+  return (
+    <div className="usuario">
+      <img
+        data-test="profile-image"
+        src={foto}
+        alt="imagem de perfil"
+        onClick={trocarFoto}
+      />
+      <div className="texto">
+        <span>
+          <strong data-test="name">{nomeDoUsuario}</strong>
+          <ion-icon
+            data-test="edit-name"
+            name="pencil"
+            onClick={trocaNomeDoUsuario}
+          ></ion-icon>
+        </span>
+      </div>
+    </div>
+  );
 }
