92445693313dfeabc108c7c8963898297aecdd5b
SolForte <mateusbrandao1234@gmail.com>
2023-02-11 15:00:47 -0300

    Feat: O nome e foto do  usuário (componente Usuario) acima das sugestões agora vem de props

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,9 +1,27 @@
 import Sugestoes from "./Sugestoes";
 import Usuario from "./Usuario";
+import { useState } from "react";
 export default function SideBar() {
+  const fotoInicial = "assets/img/profiles/catanacomics.svg";
+  const [nomeDoUsuario, setNomeDoUsuario] = useState("catanacomics");
+  const [foto, setFoto] = useState(fotoInicial);
+  
+  const trocaNomeDoUsuario=()=>{
+    const novoNome = prompt("Qual é o seu nome?");
+    if (novoNome.length !== 0) {
+        setNomeDoUsuario(novoNome);
+    }
+  }
+  
+  const trocarFoto=()=>{
+      const novaFoto = prompt("Insira o link da sua nova foto");
+      if (novaFoto.length !== 0) {
+        setFoto(novaFoto);
+    }
+  }
   return (
     <div className="sidebar">
-      <Usuario />
+      <Usuario nomeDoUsuario={nomeDoUsuario} foto={foto} trocarFoto={trocarFoto} trocaNomeDoUsuario={trocaNomeDoUsuario}/>
 
       <Sugestoes />
 
--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,39 +1,19 @@
-import { useState } from "react";
-export default function Usuario() {
-  //Feito baseado no DrivenFlix
-  const fotoInicial = "assets/img/profiles/catanacomics.svg";
-  const [nomeDoUsuario, setNomeDoUsuario] = useState("catanacomics");
-  const [foto, setFoto] = useState(fotoInicial);
-
-  function trocaNomeDoUsuario() {
-    const novoNome = prompt("Qual é o seu nome?");
-    if (novoNome.length !== 0) {
-      setNomeDoUsuario(novoNome);
-    }
-  }
-
-  function trocarFoto() {
-    const novaFoto = prompt("Insira o link da sua nova foto");
-    if (novaFoto.length !== 0) {
-      setFoto(novaFoto);
-    }
-  }
-
+export default function Usuario(props) {
   return (
     <div className="usuario">
       <img
         data-test="profile-image"
-        src={foto}
+        src={props.foto}
         alt="imagem de perfil"
-        onClick={trocarFoto}
+        onClick={props.trocarFoto}
       />
       <div className="texto">
         <span>
-          <strong data-test="name">{nomeDoUsuario}</strong>
+          <strong data-test="name">{props.nomeDoUsuario}</strong>
           <ion-icon
             data-test="edit-name"
             name="pencil"
-            onClick={trocaNomeDoUsuario}
+            onClick={props.trocaNomeDoUsuario}
           ></ion-icon>
         </span>
       </div>
